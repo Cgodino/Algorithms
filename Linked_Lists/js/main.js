74183ef920b98f526bef3247435ab2d2d364faf9
Cgodino <cgodino@gmail.com>
2017-06-30 21:41:31 -0700

    linked list 2

--- Linked_Lists/js/main.js
+++ Linked_Lists/js/main.js
@@ -1,50 +1,88 @@
-function ListNode(value) {
+// basic node constructor
+function ListNode (value) {
   this.val = value;
   this.next = null;
 };
-
-function SList() {
+// basic linked list constructor
+function SList () {
   this.head = null;
+  this._lenght = 0;
 };
-
-SList.prototype.addFront = function(value) {
+// Adding addFront prototype to linked list
+SList.prototype.addFront = function (value) {
   var node = new ListNode(value);
   var currentNode = this.head;
-  // 1st use-case: an empty list
+  this._lenght += 1;
   if (!currentNode) {
     this.head = node;
     return node;
-  };
-  // 2nd use-case: a non-empty list
+  }
   node.next = this.head;
   this.head = node;
   return node;
-  // while (currentNode.next) {
-  //   currentNode = currentNode.next;
-  // };
-  // currentNode.next = node;
-  // return node;
 };
-
-SList.prototype.removeFront = function() {
+// Adding removeFront prototype to linked list
+SList.prototype.removeFront = function () {
+  var node = this.head;
+  this._lenght -= 1;
+  if (node === null) {
+    return node;
+  }
+  this.head = node.next;
   return node;
 };
-
-SList.prototype.valueFront = function() {
-  return this.vale;
+// Adding lenght prototype
+SList.prototype.mylenght = function () {
+  return this._lenght;
 };
-
-SList.prototype.printList = function() {
+// Adding valueFront prototype to linked list
+SList.prototype.valueFront = function () {
+  return this.head.val;
+};
+// Adding Contains prototype
+SList.prototype.contains = function (value) {
+  var isPresent = false;
+  var node = this.head;
+  while (node) {
+    if (node.val == value) {
+      isPresent = true;
+      break;
+    }
+    node = node.next;
+  }
+  return isPresent;
+}
+// Adding printList prototype to linked list
+SList.prototype.printList = function () {
   var currentNode = this.head;
-  while (currentNode.next) {
-    console.log(currentNode.val);
+  var result = "";
+  while (currentNode) {
+    result += currentNode.val;
     currentNode = currentNode.next;
+    if (currentNode) { result += " -> " }
   };
-  console.log(currentNode.val);
-  return this.head;
+  return result;
 };
 
+// Testing linked list
 SingleList = new SList();
-SingleList.addFront("Carlos");
-SingleList.addFront("Mateo");
-SingleList.printList();
+SingleList.addFront("Madrid");
+SingleList.addFront("Berlin");
+SingleList.addFront("Los Angeles");
+console.log(SingleList.printList());
+console.log(SingleList.mylenght());
+SingleList.removeFront();
+console.log(SingleList.printList());
+SingleList.removeFront();
+console.log(SingleList.printList());
+SingleList.removeFront();
+console.log(SingleList.printList());
+console.log(SingleList.mylenght());
+SingleList.addFront("Moscu");
+console.log(SingleList.printList());
+SingleList.addFront("Tokyo");
+console.log(SingleList.printList());
+console.log(SingleList.valueFront());
+console.log(SingleList.contains("Berlin"));
+console.log(SingleList.contains("Moscu"));
+console.log(SingleList.mylenght());
