773771af6df0dd64ad1de5c816591d7ae574b920
Carlos Godino <cgodino@gmail.com>
2017-07-01 17:13:00 -0700

    Linked list 3

--- Linked_Lists/js/main.js
+++ Linked_Lists/js/main.js
@@ -11,57 +11,59 @@ function SList () {
 // Adding addFront prototype to linked list
 SList.prototype.addFront = function (value) {
   var node = new ListNode(value);
-  var currentNode = this.head;
+  var runner = this.head;
   this._lenght += 1;
-  if (!currentNode) {
+  if (!runner) {
     this.head = node;
     return node;
   }
-  node.next = this.head;
+  node.next = runner;
   this.head = node;
   return node;
 };
 // Adding addBack prototype to linked list
 SList.prototype.addBack = function (value) {
   var node = new ListNode(value);
-  var currentNode = this.head;
+  var runner = this.head;
   this._lenght += 1;
-  if (!currentNode) {
+  if (!runner) {
     this.head = node;
     return node;
   }
-  while (currentNode.next) {
-    currentNode = currentNode.next;
+  while (runner.next) {
+    runner = runner.next;
   }
-  currentNode.next = node;
+  runner.next = node;
   return node;
 };
 // Adding removeFront prototype to linked list
 SList.prototype.removeFront = function () {
-  var node = this.head;
-  if (node === null) {
-    return node;
+  var runner = this.head;
+  if (!runner) {
+    return null;
   }
   this._lenght -= 1;
-  this.head = node.next;
-  return node;
+  this.head = runner.next;
+  return runner;
 };
 // Adding removeBack prototype to linked list
 SList.prototype.removeBack = function () {
-  var currentNode = this.head;
+  var runner1 = this.head;
   var lastValue;
-  if (currentNode === null) {
-    return lastValue;
+  if (!runner1) {
+    // Empty linked list
+    return null;
   }
   this._lenght -= 1;
-  while (currentNode.next) {
-    var nextNode = currentNode.next;
-    if (nextNode.next == null) {
-      lastValue = currentNode.next.val;
-      currentNode.next = null;
+  var runner2 = runner1.next;
+  while (runner2) {
+    if (!runner2.next) {
+      lastValue = runner2.val;
+      runner1.next = null;
       return lastValue;
     }
-    currentNode = currentNode.next;
+    runner1 = runner2;
+    runner2 = runner2.next;
   }
 };
 // Adding lenght prototype
@@ -70,44 +72,117 @@ SList.prototype.mylenght = function () {
 };
 // Adding valueFront prototype to linked list
 SList.prototype.valueFront = function () {
-  var currentNode = this.head;
-  if (currentNode == null) {
+  var runner = this.head;
+  if (!runner) {
     return null;
   }
-  return currentNode.val;
+  return runner.val;
 };
 // Adding valueBack prototype to linked list
 SList.prototype.valueBack = function () {
-  var currentNode = this.head;
-  if (currentNode == null) {
+  var runner = this.head;
+  if (!runner) {
     return null;
   }
-  while (currentNode.next) {
-    currentNode = currentNode.next;
+  while (runner.next) {
+    runner = runner.next;
   }
-  return currentNode.val;
+  return runner.val;
 };
 // Adding Contains prototype
 SList.prototype.contains = function (value) {
   var isPresent = false;
-  var currentNode = this.head;
-  while (currentNode) {
-    if (currentNode.val == value) {
+  var runner = this.head;
+  while (runner) {
+    if (runner.val == value) {
       isPresent = true;
       break;
     }
-    currentNode = currentNode.next;
+    runner = runner.next;
   }
   return isPresent;
 }
+// Adding a new node before specific value node
+SList.prototype.prependVal = function (value, before) {
+  var node = new ListNode(value);
+  var runner1 = this.head;
+  this._lenght += 1;
+  if (!runner1) {
+    this.head = node;
+    return node;
+  }
+  var runner2 = runner1.next;
+  if (!runner2) {
+    this.head = node;
+    node.next = runner1;
+    return node;
+  }
+  if (runner1.val == before) {
+    node.next = runner1;
+    this.head = node;
+    return node;
+  }
+  while (runner2) {
+    if (runner2.val == before) {
+      node.next = runner1.next;
+      runner1.next = node;
+      return node;
+    }
+    runner1 = runner2;
+    runner2 = runner2.next;
+  }
+};
+// Adding a new node after specific value node
+SList.prototype.appendVal = function (value, after) {
+  var node = new ListNode(value);
+  var runner = this.head;
+  this._lenght += 1;
+  if (!runner) {
+    this.head = node;
+    return node;
+  }
+  while (runner) {
+    if (runner.val == after) {
+      node.next = runner.next;
+      runner.next = node;
+      return node;
+    }
+    runner = runner.next;
+  }
+}
+// Adding remove value fron linked list
+SList.prototype.removeVal = function (value) {
+  var runner1 = this.head;
+  if (!runner1) { return null }
+  var runner2 = runner1.next;
+  if (!runner2) {
+    if (runner1.val == value) {
+      this.head = null;
+    }
+    return this.head;
+  }
+  // At this point both runners exist
+  if (runner1.val == value) {
+    this.head = runner2;
+    return this.head;
+  }
+  while (runner2) {
+    if (runner2.val == value) {
+      runner1.next = runner2.next;
+      return this.head;
+    }
+    runner1 = runner2;
+    runner2 = runner2.next;
+  }
+}
 // Adding printList prototype to linked list
 SList.prototype.printList = function () {
-  var currentNode = this.head;
+  var runner = this.head;
   var result = "";
-  while (currentNode) {
-    result += currentNode.val;
-    currentNode = currentNode.next;
-    if (currentNode) { result += " -> " }
+  while (runner) {
+    result += runner.val;
+    runner = runner.next;
+    if (runner) { result += " -> " }
   };
   return result;
 };
@@ -129,8 +204,8 @@ console.log(SingleList.printList());
 SingleList.addFront("Tokyo");
 console.log(SingleList.printList());
 console.log(`Value Front: ${ SingleList.valueFront() }`);
-console.log(`Berlin is present: ${ SingleList.contains("Berlin") }`);
-console.log(`Moscu is present: ${ SingleList.contains("Moscu") }`);
+console.log(`Madrid is present: ${ SingleList.contains("Madrid") }`);
+console.log(`Tokyo is present: ${ SingleList.contains("Tokyo") }`);
 console.log(`Total lenght is: ${ SingleList.mylenght() }`);
 SingleList.removeBack();
 console.log(SingleList.printList());
@@ -147,3 +222,9 @@ SingleList.addFront("NYC");
 console.log(SingleList.printList());
 console.log(`Value Front: ${ SingleList.valueFront() }`);
 console.log(`Total lenght is: ${ SingleList.mylenght() }`);
+SingleList.prependVal("Arcadia","Barcelona");
+console.log(SingleList.printList());
+SingleList.appendVal("Girona","Buenos Aires");
+console.log(SingleList.printList());
+SingleList.removeVal("NYC");
+console.log(SingleList.printList());
